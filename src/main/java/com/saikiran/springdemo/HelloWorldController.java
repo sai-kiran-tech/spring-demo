49640c2ec0e9d9c9e7247c01c05d2360033c0f21
saikrishna@fullstacktek.com <saikrishna@fullstacktek.com>
2020-04-11 23:19:47 -0400

    Saikiran Spring Boot Commit

---
+++ src/main/java/com/saikiran/springdemo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.saikiran.springdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //method- "Hello World"
+
+    @GetMapping("/helloworld")
+    public String helloWorld(){
+
+        return "Hello World";
+    }
+}
