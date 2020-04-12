13d9ea58a2617917eeef193d87a9eed388a9b667
saikrishna@fullstacktek.com <saikrishna@fullstacktek.com>
2020-04-12 01:37:25 -0400

    Saikiran SpringBoot REST service Return Bean

--- src/main/java/com/saikiran/springdemo/HelloWorldController.java
+++ src/main/java/com/saikiran/springdemo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.saikiran.springdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean(){
 
         return new  HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/helloworldbean/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+
+        return new  HelloWorldBean(String.format("Hello World %s",name));
     }
 }
