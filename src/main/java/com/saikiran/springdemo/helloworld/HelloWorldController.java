7cb6c6bd5b50959625e673e23236da808f9a8e68
saikrishna@fullstacktek.com <saikrishna@fullstacktek.com>
2020-04-13 11:48:14 -0400

    Saikiran SpringBoot REST service Return Locale Internationalization

--- src/main/java/com/saikiran/springdemo/helloworld/HelloWorldController.java
+++ src/main/java/com/saikiran/springdemo/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.saikiran.springdemo.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //method- "Hello World"
 
@@ -13,6 +21,13 @@ public class HelloWorldController {
     public String helloWorld(){
 
         return "Hello World";
+    }
+
+    @GetMapping("/helloworld-internationalize")
+    public String helloWorld(@RequestHeader(name = "Accept-Language", required =false) Locale locale){
+
+        return messageSource.getMessage("good.morning.message",null,locale);
+
     }
 
     @GetMapping("/helloworldbean")
