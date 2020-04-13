6a47652ec5e0d147c95458ae345edc32412618f5
saikrishna@fullstacktek.com <saikrishna@fullstacktek.com>
2020-04-13 16:36:03 -0400

    Saikiran SpringBoot REST service Enable Customized Swagger Documentation

--- src/main/java/com/saikiran/springdemo/SwaggerConfig.java
+++ src/main/java/com/saikiran/springdemo/SwaggerConfig.java
@@ -2,16 +2,44 @@ package com.saikiran.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("saikiran", "saikiran.com", "saikiran@gmail.com");
+    private static Collection<VendorExtension> vendorExtensions =new ArrayList<>();;
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title","Awesome API Documentation","1.0","urn:tos",DEFAULT_CONTACT,"Apache 2.0","http://www.apache.org/licenses/LICENSE-2.0",vendorExtensions);
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/xml","application/json"));
+
+
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+
+    @Bean
+    public LinkDiscoverers discovers() {
+
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
     }
 }
--- src/main/java/com/saikiran/springdemo/user/User.java
+++ src/main/java/com/saikiran/springdemo/user/User.java
@@ -1,19 +1,24 @@
 package com.saikiran.springdemo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.hateoas.RepresentationModel;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All Details about the User Model")
 public class User extends RepresentationModel<User> {
 
     private  Integer id;
 
     @Size(min = 2, message = "Name Should be minimum 2 Characters Long")
+    @ApiModelProperty(notes = "Name Should be minimum 2 Characters Long")
     private  String  name;
 
     @Past(message = "Date of birth should be past date")
+    @ApiModelProperty(notes = "Date of birth should be past date")
     private  Date date;
 
     public User(Integer id, String name, Date date) {
